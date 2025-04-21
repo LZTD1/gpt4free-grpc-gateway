4eeccbb647f1876a699998acfbb6d7d5d4802773
pavlov <danil227pavlov@gmail.com>
2025-04-21 12:37:56 +0300

    add retry provider on yaml

--- services/AiService.py
+++ services/AiService.py
@@ -1,18 +1,20 @@
 import asyncio
 from logging import Logger
-import os
 from typing import Dict, List
 
-from g4f import AsyncClient
 import g4f
+from g4f import AsyncClient
+from g4f.Provider import ProviderUtils
+from g4f.providers.retry_provider import RetryProvider
 
 MAX_MSG_HISTORY_LEN = 500
+
 
 class AiService:
     def __init__(self, config, log: Logger):
         self.users: Dict[int, List[Dict[str, str]]] = {}
         self.client = AsyncClient()
-        self.model = config['model']
+        self.model = config['text_generation']['model']
         self.image_model = config['image_model']
         self.retry_policy = config['retry_policy']
         self.log = log
@@ -20,6 +22,10 @@ class AiService:
             "role": "system",
             "content": config["sys_promt"]
         }
+        self.text_providers = None
+        if "providers" in config["text_generation"]:
+            providers = config["text_generation"]["providers"].split(",")
+            self.text_providers = [ProviderUtils.convert[name] for name in providers]
 
     def create_user(self, uid: int):
         if uid not in self.users:
@@ -40,18 +46,21 @@ class AiService:
             self.clear_history(uid)
             return True
         return False
-    def change_image_model(self, model:str) -> bool:
+
+    def change_image_model(self, model: str) -> bool:
         if model in g4f.models._all_models:
             self.log.info("image model was changed {} -> {}", self.image_model, model)
             self.image_model = model
             return True
         return False
-    def change_text_model(self, model:str) -> bool:
+
+    def change_text_model(self, model: str) -> bool:
         if model in g4f.models._all_models:
             self.log.info("text model was changed {} -> {}", self.model, model)
             self.model = model
             return True
         return False
+
     async def generate_image(self, promt: str, uid: int) -> (str, bool):
         retries = 0
         self.log.info("User request generate_image {}:{}...", uid, promt[:20])
@@ -81,6 +90,7 @@ class AiService:
         self.log.warning(
             f"After {self.retry_policy['retry_count']} attempts, failed to receive a response from the server")
         return "Произошла ошибка сервера!", False
+
     def get_stats(self) -> (str, str):
         """
         returns chat model, image_model
@@ -124,18 +134,25 @@ class AiService:
             f"After {self.retry_policy['retry_count']} attempts, failed to receive a response from the server")
         self.add_message(uid, "assistant", "error while suggesting.")
         return "Произошла ошибка сервера!", False
+
     async def _suggest(self, uid: int) -> tuple:
+
         try:
+            prov = None
+            if self.text_providers is not None:
+                prov = RetryProvider(self.text_providers)
             response = await self.client.chat.completions.create(
                 model=self.model,
                 messages=self.users[uid],
                 web_search=False,
+                provider=prov,
             )
             return response, True
         except Exception as e:
             self.log.error("Unexpected error in function _suggest {}", e)
-            return {"choices": [{"message": {"content": "Внутренняя ошибка сервера"}}]}, False
-    async def _generate_image(self, promt:str):
+            raise e
+
+    async def _generate_image(self, promt: str):
         try:
             response = await self.client.images.generate(
                 model=self.image_model,
